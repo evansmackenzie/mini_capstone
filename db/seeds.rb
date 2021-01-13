692b37029cb98a584201384770abeee341e70c39
mackenzie <evans.mackenzie@gmail.com>
2021-01-13 15:28:37 -0500

    adds 3 products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#product = Product.create(name: "spring_toy", price: 3, description: "Helen's favorite toy")
+
+#product = Product.create(name: "mousey", price: 2, description: "Helen's second favorite toy", image_url: "")
+
+#product = Product.create(name: "pen", price: 1, description: "Lucy's favorite toy", image_url: "")

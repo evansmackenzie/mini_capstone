99107ad082f937045832b1e6099e3c7de17a9fcb
mackenzie <evans.mackenzie@gmail.com>
2021-01-14 21:16:15 -0500

    adds image_url links

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@
 #product = Product.create(name: "mousey", price: 2, description: "Helen's second favorite toy", image_url: "")
 
 #product = Product.create(name: "pen", price: 1, description: "Lucy's favorite toy", image_url: "")
+
+#product = Product.find_by(name: "pen")
+#product.image_url = "https://www.google.com/aclk?sa=l&ai=DChcSEwjqndbc7pzuAhVU_-MHHXgSCpcYABANGgJ5bQ&sig=AOD64_2NX-garmBoBr7kriII2iQMMJN12Q&adurl&ctype=5&ved=2ahUKEwiepcrc7pzuAhUJed8KHbsZCosQvhd6BAgBEH0"
+#product.save
